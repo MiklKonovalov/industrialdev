7699f9be54907e393ff35e2c7fe9e64087eb34ef
rwhart <moscow89@yandex.ru>
2021-06-17 12:10:00 +0300

    Homework 3

--- Navigation/Table/ProfileTableHederView.swift
+++ Navigation/Table/ProfileTableHederView.swift
@@ -20,16 +20,6 @@ class ProfileTableHeaderView: UITableViewHeaderFooterView {
         return spaceView
         }()
     
-    private let userName: UILabel = {
-        let userName = UILabel()
-        userName.font = UIFont.systemFont(ofSize: 18, weight: .bold)
-        userName.text = "Ice Cream"
-        userName.textAlignment = .center
-        userName.textColor = .black
-        userName.translatesAutoresizingMaskIntoConstraints = false
-        return userName
-        }()
-    
     private let newStatus: UITextField = {
         let newStatus = UITextField()
         newStatus.layer.borderWidth = 1
@@ -42,36 +32,26 @@ class ProfileTableHeaderView: UITableViewHeaderFooterView {
         return newStatus
         }()
     
-    private let currentStatus: UILabel = {
-        let currentStatus = UILabel()
-        currentStatus.font = UIFont.systemFont(ofSize: 14, weight: .regular)
-        currentStatus.text = "Waiting for something..."
-        currentStatus.textColor = .gray
-        currentStatus.textAlignment = .center
-        currentStatus.translatesAutoresizingMaskIntoConstraints = false
-        return currentStatus
-        }()
-    
     private let actionButton: UIButton = {
-     let actionButton = UIButton()
-     actionButton.layer.shadowRadius = 4
-     actionButton.layer.shadowColor = UIColor.black.cgColor
-     actionButton.layer.shadowOffset = CGSize(width: 4, height: 4)
-     actionButton.layer.shadowOpacity = 0.7
-     actionButton.layer.cornerRadius = 4
-     actionButton.clipsToBounds = false
-     actionButton.backgroundColor = .systemBlue
-     actionButton.setTitleColor(.white, for: .normal)
-     actionButton.setTitle("Show status", for: .normal)
-     actionButton.translatesAutoresizingMaskIntoConstraints = false
-     actionButton.addTarget(self, action: #selector(actionButtonPressed), for: .touchUpInside)
-    return actionButton
-    }()
+        let actionButton = UIButton()
+        actionButton.layer.shadowRadius = 4
+        actionButton.layer.shadowColor = UIColor.black.cgColor
+        actionButton.layer.shadowOffset = CGSize(width: 4, height: 4)
+        actionButton.layer.shadowOpacity = 0.7
+        actionButton.layer.cornerRadius = 4
+        actionButton.clipsToBounds = false
+        actionButton.backgroundColor = .systemBlue
+        actionButton.setTitleColor(.white, for: .normal)
+        actionButton.setTitle("Show status", for: .normal)
+        actionButton.translatesAutoresizingMaskIntoConstraints = false
+        actionButton.addTarget(self, action: #selector(actionButtonPressed), for: .touchUpInside)
+        return actionButton
+        }()
         
     //MARK: Create Actions
     @objc private func actionButtonPressed() {
-        currentStatus.text = statusText
-        print(currentStatus.text ?? "no status")
+        //currentStatus.text = statusText
+        //print(currentStatus.text ?? "no status")
     }
     
     @objc private func statusTextChange(_ newStatus: UITextField) {
@@ -91,31 +71,19 @@ class ProfileTableHeaderView: UITableViewHeaderFooterView {
     
     //MARK: Add subviews
     fileprivate func setupViews() {
-        contentView.addSubview(userName)
         contentView.addSubview(newStatus)
-        contentView.addSubview(currentStatus)
+
         contentView.addSubview(actionButton)
         
         newStatus.leftView = spaceView
         contentView.addSubview(spaceView)
         
     //MARK: Setup constraints
-        userName.snp.makeConstraints { make in
-            make.top.equalToSuperview().multipliedBy(27)
-            make.centerX.equalToSuperview()
-            make.height.equalTo(40)
-        }
-        
-        currentStatus.snp.makeConstraints { make in
-            make.top.equalTo(userName.snp.bottom).offset(20)
-            make.left.equalTo(userName.snp.left).offset(0)
-        }
         
         newStatus.snp.makeConstraints { make in
-            make.top.equalTo(currentStatus.snp.bottom).offset(20)
-            make.left.equalTo(userName.snp.left)
+            make.bottom.equalTo(actionButton.snp.top).offset(-20)
+            make.left.equalToSuperview().offset(200)
             make.trailing.equalToSuperview().offset(-16)
-            //make.right.equalTo(contentView.snp.right).offset(-16)
             make.height.equalTo(40)
         }
             
@@ -127,12 +95,11 @@ class ProfileTableHeaderView: UITableViewHeaderFooterView {
         }
         
         actionButton.snp.makeConstraints { make in
-            make.top.equalTo(newStatus.snp.bottom).offset(20)
+            make.bottom.equalToSuperview().offset(-20)
             make.left.equalTo(contentView.snp.left).offset(16)
             make.right.equalTo(contentView.snp.right).offset(-16)
         }
             
 }
  
-
 }
