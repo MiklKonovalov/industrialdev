12c2495032eb34b48b9b01fce6a7aba7c86c6bbe
rwhart <moscow89@yandex.ru>
2021-08-30 23:16:03 +0300

    JSONSerialization and JSON Decodable

---
+++ Navigation/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constant.swift
+//  Navigation
+//
+//  Created by Misha on 30.08.2021.
+//
+
+import Foundation
+import UIKit
+
+class Constants {
+    
+    public enum Constants: String {
+        
+        static let localURL = "http://localhost:5050"
+
+    }
+    
+}
---
+++ Navigation/Model/Planet.swift
@@ -0,0 +1,8 @@
+//
+//  Planet.swift
+//  Navigation
+//
+//  Created by Misha on 30.08.2021.
+//
+
+import Foundation
