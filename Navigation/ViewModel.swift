f2ff3e67429f072da6824e67af93619445b19313
rwhart <moscow89@yandex.ru>
2021-09-03 19:41:18 +0300

    Add Factory for LoginInspector

---
+++ Navigation/ViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  ViewModels.swift
+//  Navigation
+//
+//  Created by Misha on 02.09.2021.
+//
+
+import Foundation
+import UIKit
+import Firebase
+
+class EmployeesViewModel: NSObject {
+    
+}
